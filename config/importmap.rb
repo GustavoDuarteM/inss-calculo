a253564349f088196afcd923fd11e6edd6ca9979
Gustavo Duarte <gustavo_dmuniz@hotmail.com>
2024-11-07 20:40:06 -0300

    pin uuid in inportmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@rails/actioncable", to: "actioncable.esm.js"
 pin 'inputmask', to: 'https://ga.jspm.io/npm:inputmask@5.0.8/dist/inputmask.js'
+pin "uuid" # @11.0.2
