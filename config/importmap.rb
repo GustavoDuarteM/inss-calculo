eedbfb47e60c85fa1e4b14a59b0fe809dac17c9a
Gustavo Duarte <gustavo_dmuniz@hotmail.com>
2024-11-07 19:53:32 -0300

    Setup Action Cable

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@rails/actioncable", to: "actioncable.esm.js"
 pin 'inputmask', to: 'https://ga.jspm.io/npm:inputmask@5.0.8/dist/inputmask.js'
